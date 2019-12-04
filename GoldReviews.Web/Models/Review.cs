ef7a54d8bdcc488c8225eac3f748153bd4ee23de
Nurbolat <Nurbolat@X571GD>
2019-12-04 16:22:26 +0600

    Remove redundant files

--- GoldReviews.Web/Models/Review.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GoldReviews.Web.Models
-{
-    public class Review
-    {
-        public int Id { get; set; }
-        public string ClientName { get; set; }
-        public string ReviewText { get; set; }
-    }
-}
